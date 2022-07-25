d995c2de3c62d8629789eec06178442cc12c24d0
Guillaume Deconinck <gui.deconinck@gmail.com>
2022-07-25 09:25:56 +0200

    feat: correctly parse monkey lang

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -35,40 +35,262 @@ impl Lexer {
         self.next_read_pos += 1;
     }
 
+    fn peak_char(&self) -> Option<char> {
+        if self.next_read_pos >= self.input.len() {
+            None
+        } else {
+            Some(
+                *self
+                    .input
+                    .get(self.next_read_pos)
+                    .expect("Wrong read position"),
+            )
+        }
+    }
+
+    fn skip_whitespaces(&mut self) {
+        while let Some(current_char) = self.current_char {
+            if current_char != ' '
+                && current_char != '\t'
+                && current_char != '\n'
+                && current_char != '\r'
+            {
+                break;
+            }
+            self.read_char();
+        }
+    }
+
+    fn read_identifier(&mut self) -> String {
+        let mut identifier = String::new();
+
+        while let Some(current_char) = self.current_char {
+            if Lexer::is_letter(current_char) {
+                identifier.push(current_char);
+                self.read_char();
+            } else {
+                break;
+            }
+        }
+
+        identifier
+    }
+
+    fn read_number(&mut self) -> String {
+        let mut identifier = String::new();
+
+        while let Some(current_char) = self.current_char {
+            if Lexer::is_number(current_char) {
+                identifier.push(current_char);
+                self.read_char();
+            } else {
+                break;
+            }
+        }
+
+        identifier
+    }
+
+    fn is_letter(c: char) -> bool {
+        ('a'..='z').contains(&c) || ('A'..='Z').contains(&c) || (c == '_')
+    }
+
+    fn is_number(c: char) -> bool {
+        ('0'..='9').contains(&c)
+    }
+
+    fn is_multi_chars_symbol(c: char, second_char_opt: Option<char>) -> bool {
+        if let Some(second_char) = second_char_opt {
+            if c == '!' && second_char == '=' {
+                true
+            } else {
+                c == '=' && second_char == '='
+            }
+        } else {
+            false
+        }
+    }
+
     // TODO: to convert to an actual Iterator
     pub fn next_token(&mut self) -> Option<Token> {
+        self.skip_whitespaces();
+
         let current_char = self.current_char?;
 
-        let token_type = TokenType::from(current_char);
+        let token_string = if Lexer::is_letter(current_char) {
+            self.read_identifier()
+        } else if Lexer::is_number(current_char) {
+            self.read_number()
+        } else if Lexer::is_multi_chars_symbol(current_char, self.peak_char()) {
+            let mut string = String::from(current_char);
+            self.read_char();
+            let second_char = self.current_char?;
+            self.read_char();
+            string.push(second_char);
+            string
+        } else {
+            let string = String::from(current_char);
+            self.read_char();
+            string
+        };
+
+        let token_type = TokenType::from(token_string.as_str());
         let token = Token {
             token_type,
             literal: current_char,
         };
 
-        self.read_char();
         Some(token)
     }
 }
 
-#[test]
-fn test_lexer() {
-    let input = String::from("=+(){{}},;");
+#[cfg(test)]
+mod tests {
+    use crate::{lexer::Lexer, token::TokenType};
 
-    struct TestCase(TokenType, char);
+    #[test]
+    fn test_lexer_simple_chars() {
+        let input = String::from("=+(){},;");
 
-    let test_cases = [
-        TestCase(TokenType::ASSIGN, '='),
-        TestCase(TokenType::PLUS, '+'),
-        TestCase(TokenType::LPAREN, '('),
-        TestCase(TokenType::RPAREN, ')'),
-    ];
+        struct TestCase(TokenType, char);
 
-    let mut lexer = Lexer::new(input);
+        let test_cases = [
+            TestCase(TokenType::Assign, '='),
+            TestCase(TokenType::Plus, '+'),
+            TestCase(TokenType::Lparen, '('),
+            TestCase(TokenType::Rparent, ')'),
+            TestCase(TokenType::Lbrace, '{'),
+            TestCase(TokenType::Rbrace, '}'),
+            TestCase(TokenType::Comma, ','),
+            TestCase(TokenType::Semicolon, ';'),
+        ];
 
-    for case in test_cases.into_iter() {
-        let token = lexer.next_token().unwrap();
+        let mut lexer = Lexer::new(input);
 
-        assert_eq!(token.token_type, case.0);
-        assert_eq!(token.literal, case.1);
+        for case in test_cases.into_iter() {
+            let token = lexer.next_token().unwrap();
+
+            println!("{:?}", &token);
+
+            assert_eq!(token.token_type, case.0);
+            assert_eq!(token.literal, case.1);
+        }
+    }
+
+    #[test]
+    fn test_lexer_real_1() {
+        let input = String::from(
+            "
+        let five = 5;
+        let ten = 10;
+
+        let add = fn(x, y) {
+            x + y;
+        };
+
+        let result = add(five, ten);
+
+        !-/*5;
+        5 < 10 > 5;
+
+        if (5 < 10) {
+            return true;
+        } else {
+            return false;
+        }
+
+        10 == 10;
+        10 != 9;
+        ",
+        );
+
+        struct TestCase(TokenType, char);
+
+        let test_cases = [
+            TestCase(TokenType::Let, 'l'),
+            TestCase(TokenType::Ident(String::from("five")), 'f'),
+            TestCase(TokenType::Assign, '='),
+            TestCase(TokenType::Int(5), '5'),
+            TestCase(TokenType::Semicolon, ';'),
+            TestCase(TokenType::Let, 'l'),
+            TestCase(TokenType::Ident(String::from("ten")), 't'),
+            TestCase(TokenType::Assign, '='),
+            TestCase(TokenType::Int(10), '1'),
+            TestCase(TokenType::Semicolon, ';'),
+            TestCase(TokenType::Let, 'l'),
+            TestCase(TokenType::Ident(String::from("add")), 'a'),
+            TestCase(TokenType::Assign, '='),
+            TestCase(TokenType::Function, 'f'),
+            TestCase(TokenType::Lparen, '('),
+            TestCase(TokenType::Ident(String::from("x")), 'x'),
+            TestCase(TokenType::Comma, ','),
+            TestCase(TokenType::Ident(String::from("y")), 'y'),
+            TestCase(TokenType::Rparent, ')'),
+            TestCase(TokenType::Lbrace, '{'),
+            TestCase(TokenType::Ident(String::from("x")), 'x'),
+            TestCase(TokenType::Plus, '+'),
+            TestCase(TokenType::Ident(String::from("y")), 'y'),
+            TestCase(TokenType::Semicolon, ';'),
+            TestCase(TokenType::Rbrace, '}'),
+            TestCase(TokenType::Semicolon, ';'),
+            TestCase(TokenType::Let, 'l'),
+            TestCase(TokenType::Ident(String::from("result")), 'r'),
+            TestCase(TokenType::Assign, '='),
+            TestCase(TokenType::Ident(String::from("add")), 'a'),
+            TestCase(TokenType::Lparen, '('),
+            TestCase(TokenType::Ident(String::from("five")), 'f'),
+            TestCase(TokenType::Comma, ','),
+            TestCase(TokenType::Ident(String::from("ten")), 't'),
+            TestCase(TokenType::Rparent, ')'),
+            TestCase(TokenType::Semicolon, ';'),
+            TestCase(TokenType::Bang, '!'),
+            TestCase(TokenType::Minus, '-'),
+            TestCase(TokenType::Slash, '/'),
+            TestCase(TokenType::Asterisk, '*'),
+            TestCase(TokenType::Int(5), '5'),
+            TestCase(TokenType::Semicolon, ';'),
+            TestCase(TokenType::Int(5), '5'),
+            TestCase(TokenType::Lt, '<'),
+            TestCase(TokenType::Int(10), '1'),
+            TestCase(TokenType::Gt, '>'),
+            TestCase(TokenType::Int(5), '5'),
+            TestCase(TokenType::Semicolon, ';'),
+            TestCase(TokenType::If, 'i'),
+            TestCase(TokenType::Lparen, '('),
+            TestCase(TokenType::Int(5), '5'),
+            TestCase(TokenType::Lt, '<'),
+            TestCase(TokenType::Int(10), '1'),
+            TestCase(TokenType::Rparent, ')'),
+            TestCase(TokenType::Lbrace, '{'),
+            TestCase(TokenType::Return, 'r'),
+            TestCase(TokenType::True, 't'),
+            TestCase(TokenType::Semicolon, ';'),
+            TestCase(TokenType::Rbrace, '}'),
+            TestCase(TokenType::Else, 'e'),
+            TestCase(TokenType::Lbrace, '{'),
+            TestCase(TokenType::Return, 'r'),
+            TestCase(TokenType::False, 'f'),
+            TestCase(TokenType::Semicolon, ';'),
+            TestCase(TokenType::Rbrace, '}'),
+            TestCase(TokenType::Int(10), '1'),
+            TestCase(TokenType::Eq, '='),
+            TestCase(TokenType::Int(10), '1'),
+            TestCase(TokenType::Semicolon, ';'),
+            TestCase(TokenType::Int(10), '1'),
+            TestCase(TokenType::Neq, '!'),
+            TestCase(TokenType::Int(9), '9'),
+            TestCase(TokenType::Semicolon, ';'),
+        ];
+
+        let mut lexer = Lexer::new(input);
+
+        for case in test_cases.into_iter() {
+            let token = lexer.next_token().unwrap();
+
+            println!("{:?}", &token);
+
+            assert_eq!(token.token_type, case.0);
+            assert_eq!(token.literal, case.1);
+        }
     }
 }
--- src/token/mod.rs
+++ src/token/mod.rs
@@ -1,44 +1,85 @@
+use once_cell::sync::Lazy;
+use regex::Regex;
+
 /// The different types of tokens in the Monkey language
 #[derive(PartialEq, Eq, Debug)]
 pub enum TokenType {
-    ILLEGAL,
-    EOF,
+    Illegal,
+    Eof,
 
-    IDENT(String),
-    INT(i64),
+    Ident(String),
+    Int(i32),
 
-    ASSIGN,
-    PLUS,
+    Assign,
+    Plus,
+    Minus,
+    Eq,
+    Neq,
 
-    COMMA,
-    SEMICOLON,
+    Comma,
+    Semicolon,
+    Bang,
+    Asterisk,
+    Slash,
 
-    LPAREN,
-    RPAREN,
-    LBRACE,
-    RBRACE,
+    Lparen,
+    Rparent,
+    Lbrace,
+    Rbrace,
+    Lt,
+    Gt,
 
     // Incomplete
-    FUNCTION,
-    LET,
+    Function,
+    Let,
+    If,
+    Else,
+    Return,
+    True,
+    False,
 }
 
-impl From<char> for TokenType {
-    fn from(to_parse: char) -> Self {
+impl From<&str> for TokenType {
+    fn from(to_parse: &str) -> Self {
         match to_parse {
-            '=' => TokenType::ASSIGN,
-            '+' => TokenType::PLUS,
-            '(' => TokenType::LPAREN,
-            ')' => TokenType::RPAREN,
-            // TODO: to complete
-            _ => TokenType::ILLEGAL,
+            "=" => TokenType::Assign,
+            "+" => TokenType::Plus,
+            "-" => TokenType::Minus,
+            "==" => TokenType::Eq,
+            "!=" => TokenType::Neq,
+            "(" => TokenType::Lparen,
+            ")" => TokenType::Rparent,
+            "{" => TokenType::Lbrace,
+            "}" => TokenType::Rbrace,
+            "," => TokenType::Comma,
+            ";" => TokenType::Semicolon,
+            "!" => TokenType::Bang,
+            "*" => TokenType::Asterisk,
+            "/" => TokenType::Slash,
+            "<" => TokenType::Lt,
+            ">" => TokenType::Gt,
+            "" => TokenType::Eof,
+            "let" => TokenType::Let,
+            "fn" => TokenType::Function,
+            "if" => TokenType::If,
+            "else" => TokenType::Else,
+            "return" => TokenType::Return,
+            "true" => TokenType::True,
+            "false" => TokenType::False,
+            _ if REGEX_IDENTIFIER.is_match(to_parse) => TokenType::Ident(to_parse.into()),
+            _ if REGEX_INT.is_match(to_parse) => TokenType::Int(str::parse(to_parse).unwrap()),
+            _ => TokenType::Illegal,
         }
     }
 }
 
+static REGEX_IDENTIFIER: Lazy<Regex> = Lazy::new(|| Regex::new("[a-zA-Z_]+").unwrap());
+static REGEX_INT: Lazy<Regex> = Lazy::new(|| Regex::new("[0-9]+").unwrap());
+
 /// A specific token
 ///
 /// Not sure it will stays in the long run
+#[derive(Debug)]
 pub struct Token {
     pub token_type: TokenType,
     pub literal: char,
